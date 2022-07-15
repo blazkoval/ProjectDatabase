8cbb223b806bdd6ad0aa1ebc695dfda6ca2b5ed5
blazkoval <blazkovalenka2@gmail.com>
2022-07-15 07:51:20 +0200

    finall changes

--- README.md
+++ README.md
@@ -6,15 +6,14 @@ Definujte datové struktury (PostgreSQL + SQLAlchemy) pro ukládání dostupnýc
 ## Postup
 ### grafické zpracování struktur
 ##### [grafické schéma](ProjectDatabase-main/schematabulek.pdf)
-Pomocí stránky dbdiagram.io (https://dbdiagram.io/home) jsme si vytvořili vizualizaci datových strukutr. Definovali jsme si hlavní tabulky: Predmet, Zkouska, Studijni_skupina, Uzivatel, Opravneni. Mezi tabulkami, které mají mezi sebou vztah N:N, jsme vytvořili dodatečné tabulky, které nesou pouze primární klíče hlavních tabulek. 
+Pomocí stránky dbdiagram.io (https://dbdiagram.io/home) jsme si vytvořili vizualizaci datových strukutr. Definovali jsme si hlavní tabulky: Predmet, Zkouska, Studijni_skupina, Uzivatel, Opravneni. Mezi tabulkami, které mají mezi sebou vztah M:N, jsme vytvořili dodatečné tabulky, které nesou pouze primární klíče hlavních tabulek. 
 ### definování struktur
 ##### [SQLAlchemy](ProjectDatabase-main/Zkousky/mainjl.ipynb#SQLAlchemy)
-Vytvořili jsme modely datových entit za využití SQLAlchemy knihovny. Hlavní tabulky jsou deklarovány jako třídy (class), mezitabulky jsou vytvořeny pomocí funkce Table. Původně vytvářeno jako main.py (ve VisualStudio Code), ale kvůli problému s porpojením s Postgresem vytvářeno jako mainjl.ipynb (v JupyterLabu)
+Vytvořili jsme modely datových entit za využití SQLAlchemy knihovny. Hlavní tabulky jsou deklarovány jako třídy (class), mezitabulky jsou vytvořeny pomocí funkce Table. Původně vytvářeno jako main.py (ve VisualStudio Code), ale kvůli problému s porpojením s Postgresem vytvářeno jako notebook mainjl.ipynb (v JupyterLabu)
 ### testování
-Abych zjistila, jsetli mám tabulky správně definované, tak jsem si nejprve nahrála data na server. Propojení databáze se serverem jsem vytvořila pomocí connectionstringu. Naplnila jsem struktury náhodnými daty. K tomu jsem využila CRUD ops. V pgAdminu se mi poté zobrazily jednotlivé entity, které jsem nahrála. Tím se ověřila správnost definování struktur.
+Abych zjistila, jestli mám tabulky správně definované, tak jsem si nejprve nahrála data na server. Propojení databáze se serverem jsem vytvořila pomocí connectionstringu. Naplnila jsem struktury náhodnými daty. K tomu jsem využila CRUD ops. V pgAdminu se mi poté zobrazily jednotlivé entity, které jsem nahrála. Tím se ověřila správnost definování struktur.
 ### navázání na databázové záznamy
--znovu vytváření struktur pro GraphQL, jak se definují vztahy? explicitní/implicitní resolvery
-
-#### Budoucí termíny
-- 	8. 6. 2022 projektový den
-- 	30. 6. 2022 uzavření projektu
+#### [GraphQL](ProjectDatabase-main/Zkousky/mainjl.ipynb#GraphQL)
+V GrapgQL jsem si znovu nadefinovala modely, které navazují na datové struktury v SQLAlchemy. Díky tomu lze databázi spustit v aplikaci GraphiQL. Zde je možnost číst záznamy v databázy.
+### kontejnerizace
+Posledním krokem bylo vytvořit program, izolovat aplikace se všemi knihovnamy do kontejnerů. 
--- uois__/pyf/main.py
+++ uois__/pyf/main.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ----- docker-compose -> compose up
-# ---- requirements
-
-# ## SQLAlchemy
-
-# ### Models
+# # SQLAlchemy
+ 
+# ## Models
 
 from sqlalchemy import Column, String, BigInteger, Integer, DateTime, ForeignKey, Sequence, Table, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_utils.functions import database_exists, create_database
 
-connectionstring = 'postgresql+psycopg2://postgres:example@postgres/ProjectDatabase'
-if not database_exists(connectionstring):  #=> False
+"""
+Vytvoření connection stringu k úplnému propojení databáze se serverem
+"""
+connectionstring = 'postgresql+psycopg2://postgres:example@postgres/data'
+if not database_exists(connectionstring): 
     try:
         create_database(connectionstring)
         doCreateAll = True
@@ -29,8 +29,11 @@ else:
 BaseModel = declarative_base()
 unitedSequence = Sequence('all_id_seq')
 
-#Zkouska_Zkousejici
-Examiner_Exam = Table('examiner_groups', BaseModel.metadata,
+"""
+Tabulky propojující modely, které mají mezi sebou relaci M:N. Vyváří se pomocí funkce Table.
+"""
+#Zkouska_Zkousejici 
+Examiner_Exam = Table('examiner_groups', BaseModel.metadata, 
                       Column('exam_id', ForeignKey('exam.id'), primary_key=True),
                       Column('examiner_id', ForeignKey('user.id'), primary_key=True)
                       )
@@ -39,19 +42,43 @@ Student_Exam = Table('student_groups', BaseModel.metadata,
                      Column('exam_id', ForeignKey('exam.id'), primary_key=True),
                      Column('student_id', ForeignKey('user.id'), primary_key=True)
                      )
-
+   
 #Studijni_skupina_Predmet
 StudyGroup_Subject = Table('subject_study_groups', BaseModel.metadata,
                            Column('subject_id', ForeignKey('subject.id'), primary_key=True),
                            Column('study_group_id', ForeignKey('study_group.id'), primary_key=True)
                            )
 
-#Zkouska
+
 class ExamModel(BaseModel):
+    """
+    Třída reprezentující model zkoušky.
+    
+    Atributy:
+    ---------
+    id
+        primární klíč
+    name
+        název: zkouška, zápočet nebo klasifikovaný zápočet
+    exam_date
+        termín zkoušky ve formátu YYYY-MM-DD hh:mm:ss
+    student_capacity
+        maximální kapacita studentů
+    additional_information
+        poznámka
+    id_subject
+        přiřazení předmětu zkoušce skrz primární klíč SubjectModel
+    examiners
+        relace mezi zkouškou a zkoušejícím (M:N) - odkazuje na tabulku Examiner_Exam
+    students
+        relace mezi zkouškou a studentem (M:N) - odkazuje na tabulku Student_Exam
+    subjects
+        relace mezi zkouškou a předmětem (N:1)
+    """
     __tablename__ = 'exam'
     id = Column(BigInteger, Sequence('all_id_seq'), primary_key=True)
-    name = Column(String) #zkouska/zapocet/zapoctovy test..
-    id_examiner = Column(BigInteger, ForeignKey('user.id'))
+    name = Column(String) #zkouska/zapocet/klasifikovany zapocet..
+    #id_examiner = Column(BigInteger, ForeignKey('user.id')) #nepouzitelne, kvuli tabulce Examiner_Exam
     exam_date = Column(DateTime)
     student_capacity = Column(Integer)
     signed_students = Column(BigInteger, ForeignKey('user.id'))    
@@ -62,8 +89,39 @@ class ExamModel(BaseModel):
     students = relationship('UserModel', secondary=Student_Exam, back_populates='exams_students')
     subject = relationship('SubjectModel', back_populates='exams')
 
-#Uzivatel
+
 class UserModel(BaseModel):
+    """
+    Třída reprezentující model uživatele.
+    
+    Atributy:
+    ---------
+    id
+        primární klíč
+    name
+        jméno uživatele
+    surname
+        příjmení uživatele
+    title
+        titul
+    email
+        email
+    phone_number
+        telefoní číslo
+    id_study_group
+        přiřazení studijní skupiny uživatelovi skrz primární klíč StudyGroupModel
+    id_authorization
+        přiřazení autorizace uživatelovi skrz primární klíč AuthorizationModel
+    exams_examiners
+        relace mezi zkouškou a zkoušejícím (M:N) - odkazuje na tabulku Examiner_Exam
+    exams_students
+        relace mezi zkouškou a studentem (M:N) - odkazuje na tabulku Student_Exam
+    study_groups
+        relace mezi uživatelem a studijní skupinou (N:1)
+    authorization
+        relace mezi uživatelem a autorizací (N:1)
+    """
+
     __tablename__ = 'user'
     id = Column(BigInteger, Sequence('all_id_seq'), primary_key=True)
     name = Column(String)
@@ -77,10 +135,24 @@ class UserModel(BaseModel):
     exams_examiners = relationship('ExamModel', secondary=Examiner_Exam, back_populates='examiners')
     exams_students = relationship('ExamModel', secondary=Student_Exam, back_populates='students')
     study_group = relationship('StudyGroupModel', back_populates='users')
-    authorization = relationship('AuthorizationModel', back_populates='user') #, uselist='False
+    authorization = relationship('AuthorizationModel', back_populates='user')
 
 #Predmet
 class SubjectModel(BaseModel):
+    """
+    Třída reprezentující model předmětu.
+    
+    Atributy:
+    ---------
+    id
+        primární klíč
+    name
+        název předmětu   
+    study_groups
+        relace mezi studijní skupinou a předmětem (M:N) - odkazuje na tabulku StudyGroup_Subject
+    exams
+        relace mezi zkouškou a studijní skupinou (N:1)
+    """
     __tablename__ = 'subject'
     id = Column(BigInteger, Sequence('all_id_seq'), primary_key=True)
     name = Column(String)
@@ -90,6 +162,20 @@ class SubjectModel(BaseModel):
     
 #Studijni_skupina    
 class StudyGroupModel(BaseModel):
+    """
+    Třída reprezentující model studijní skupiny.
+    
+    Atributy:
+    ---------
+    id
+        primární klíč
+    name
+        název studijní skupiny
+    subjects
+        relace mezi studijní skupinou a předmětem (M:N) - odkazuje na tabulku StudyGroup_Subject
+    users
+        relace mezi uživatelem a studijní skupinou (N:1)
+    """
     __tablename__ = 'study_group'
     id = Column(BigInteger, Sequence('all_id_seq'), primary_key=True)
     name = Column(String)
@@ -99,6 +185,22 @@ class StudyGroupModel(BaseModel):
 
 #Opravneni
 class AuthorizationModel(BaseModel):
+    """
+    Třída reprezentující model autorizace.
+    
+    Atributy:
+    ---------
+    id
+        primární klíč
+    web_admin
+        true pokud je id 1
+    student
+        true pokud je id 2
+    examiner   
+        true pokud je id 3
+    user
+        relace mezi uživatelem a autorizací (N:1)        
+    """
     __tablename__ = 'authorization'
     id = Column(BigInteger, Sequence('all_id_seq'), primary_key=True)
     web_admin = Column(Boolean)
@@ -109,44 +211,21 @@ class AuthorizationModel(BaseModel):
     
 from sqlalchemy import create_engine
 
+"""
+Vytvoření engine a struktury databáze a jejich tabulek
+"""
 engine = create_engine(connectionstring) 
 BaseModel.metadata.create_all(engine)
 
-# ### CRUD Ops
-#taky smazat?       +++
-from sqlalchemy.orm import sessionmaker
+# # GraphQL
 
-SessionMaker = sessionmaker(bind=engine)
-session = SessionMaker()
+# ## Models
 
-# ## GraphQL
-
-import uvicorn
-from multiprocessing import Process
-
-servers = {}
-_api_process = None
-
-def start_api(app=None, port=9992, runNew=True):
-    """Stop the API if running; Start the API; Wait until API (port) is available (reachable)"""
-    assert port in [9991, 9992, 9993, 9994], f'port has unexpected value {port}'
-    def run():
-        uvicorn.run(app, port=port, host='0.0.0.0', root_path='')    
-        
-    _api_process = servers.get(port, None)
-    if _api_process:
-        _api_process.terminate()
-        _api_process.join()
-        del servers[port]
-    
-    if runNew:
-        assert (not app is None), 'app is None'
-        _api_process = Process(target=run, daemon=True)
-        _api_process.start()
-        servers[port] = _api_process
-
-
-# ### Models
+"""
+Navázání databázových struktur využívající SQLAlchemy na databázové záznamy.
+Modely jsou definovány jako třídy. Atributy tříd jsou nazvány stejne jako v SQLAlchemy.
+Díky využití již definovaných modelů v SQLAlchemy není třeba vytvářet mezitabulky ani resolvery.
+"""
 
 import graphene
 
@@ -170,12 +249,12 @@ class UserGQL(graphene.ObjectType):
     title = graphene.String()
     email = graphene.String()
     phone_number = graphene.String()
-    id_study_group = graphene.ID() #ForeignKey
-    id_authorization = graphene.ID() #ForeignKey
+    id_study_group = graphene.ID() 
+    id_authorization = graphene.ID()
         
     exams_examiners = graphene.Field(graphene.List(lambda: ExamGQL))
     exams_students = graphene.Field(graphene.List(lambda: ExamGQL))
-    study_group = graphene.Field(lambda: StudyGroupGQL) #jedna skupina, ne mnozina skupin
+    study_group = graphene.Field(lambda: StudyGroupGQL)
     authorization = graphene.Field(lambda: AuthorizationGQL)
 
 class SubjectGQL(graphene.ObjectType):
@@ -202,6 +281,31 @@ class AuthorizationGQL(graphene.ObjectType):
 
 # ### Query Root
 class QueryGQL(graphene.ObjectType):
+    """
+    Třída, která získavá hlavní datové struktury a jsou skrz ni obsluhovány případné dotazy
+
+    Atributy:
+    ---------
+    user
+        dotaz na uživatele
+    exam
+        dotaz na zkoušku
+    subject
+        dotaz na předmět
+    study_group
+        dotaz na studijní skupinu
+                
+    Metody:
+    -------
+    resolve_user
+        implicitní resolver vrací výsledek dotazu na uživatele
+    resolve_exam
+        implicitní resolver vrací výsledek dotazu na zkoušku
+    resolve_subject
+        implicitní resolver vrací výsledek dotazu na předmět
+    resolve_study_group
+        implicitní resolver vrací výsledek dotazu na studijní skupinu
+    """
     user = graphene.Field(UserGQL, id = graphene.ID(required = True)) #pta se pouze na uzivatele
     exam = graphene.Field(ExamGQL, id = graphene.ID(required = True))
     subject = graphene.Field(SubjectGQL, id = graphene.ID(required = True))
@@ -223,6 +327,10 @@ class QueryGQL(graphene.ObjectType):
         result = session.query(StudyGroupModel).filter(StudyGroupModel.id == id).first()
         return result
     
+
+"""
+Řešení problému nejnovější verze starlette_graphene3
+"""    
 import requests
 
 def singleCache(f):
@@ -245,7 +353,10 @@ def getSwapi():
     r = requests.get(source)
     return r.text
 
-#from starlette.graphql import GraphQLApp
+
+"""
+Vytvoření finální aplikace pro čtení z databáze.
+"""
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 
 import graphene
